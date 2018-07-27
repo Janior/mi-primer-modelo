2b0ac6779cc1b18e53aef5f598e7106c90661484
Janior <janior_orlando@hotmail.com>
2018-07-27 05:21:09 -0700

    modificar migracion de movies

--- db/migrate/20180725235343_create_movies.rb
+++ db/migrate/20180725235343_create_movies.rb
@@ -2,9 +2,13 @@ class CreateMovies < ActiveRecord::Migration[5.1]
   def change
     create_table :movies do |t|
       t.string :name
+      t.integer :duration
+      t.integer: year
+      t.string :rating
+      t.text :description 
+      t.text :image_url
 
-
-      t.timestamps
+      t.timestamps null: false
     end
   end
 end
