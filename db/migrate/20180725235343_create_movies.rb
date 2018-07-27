e8c1ed913bca78acb6d28b1ef785ed6c0ee1efaa
Janior <janior_orlando@hotmail.com>
2018-07-27 05:25:13 -0700

    corregir migacion

--- db/migrate/20180725235343_create_movies.rb
+++ db/migrate/20180725235343_create_movies.rb
@@ -3,7 +3,7 @@ class CreateMovies < ActiveRecord::Migration[5.1]
     create_table :movies do |t|
       t.string :name
       t.integer :duration
-      t.integer: year
+      t.integer :year
       t.string :rating
       t.text :description 
       t.text :image_url
